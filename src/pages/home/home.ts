74ece8c6bb90d69be811ff1d8abbfbb5c3f1b4c9
Matheus Castro <matheuscastro@gmail.com>
2019-05-17 11:38:52 -0300

    Salvando os dados do usuário logado em localStorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -22,7 +22,7 @@ export class HomePage {
   login() {
     this.auth.authenticate(this.credenciais)
       .subscribe(response => {
-        console.log(response.headers.get('Authorization'));
+        this.auth.successfulLogin(response.headers.get('Authorization'));
         this.navCtrl.setRoot("CategoriasPage");
       },
       error => {});
